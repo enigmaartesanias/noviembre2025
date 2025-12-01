1b5eedcdef947fb450260ce7b67165dbb8b3a62e
AldoEnigma <artesaniasenigma@gmail.com>
2025-11-30 22:43:53 -0500

    Fix: Aumentar límite de caché de Workbox a 5MB para resolver error de build en Netlify

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,9 @@ export default defineConfig({
             type: 'image/png'
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5MB
       }
     })
   ],
